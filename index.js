20d1c3120e0b0448370550793fe8ada6332c031e
Shahzad <mech1090@gmail.com>
2022-09-18 13:02:29 -0600

    db connected

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('config')
+const res = require('express/lib/response')
+require('./db')
+
+const app = express()
+
+app.use(express())
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.send('Testing okkk')
+})
+
+app.get('*',(req,res)=>{
+    res.send('BAd_Request')
+})
+
+mongoose.connection.once('open',()=>{
+    
+    app.listen(config.get('port'),()=>{
+    console.log(`Server is running on port ${config.get('port')}`)
+})
+
+    console.log('DB CONNECTED')
+
+})
+
